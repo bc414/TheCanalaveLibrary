c87aaeec505550b248043d27b8c6456b5c48f240
Brian Cheng <bc414a@gmail.com>
2025-10-28 19:15:13 -0400

    Added sql server

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -4,11 +4,14 @@ IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(ar
 
 IResourceBuilder<RedisResource> cache = builder.AddRedis("cache");
 
+IResourceBuilder<IResourceWithConnectionString> defaultConnection = builder.AddConnectionString("DefaultConnection");
+
 // This adds your web server project to the AppHost.
 // After you rename the project to "TheCanalaveLibrary.Web",
 // you will need to update this line to reflect the new name, like this:
 // var web = builder.AddProject<Projects.TheCanalaveLibrary_Web>("thecanalavelibrary-web");
 IResourceBuilder<ProjectResource> web = builder.AddProject<Projects.TheCanalaveLibrary>("thecanalavelibrary-web")
-    .WithReference(cache);
+    .WithReference(cache)
+    .WithReference(defaultConnection);
 
 builder.Build().Run();
