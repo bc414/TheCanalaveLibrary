7586cbf8c5ffd5e3ee212b05942c16c91915d228
Brian Cheng <bc414a@gmail.com>
2026-06-13 17:41:07 -0400

    Refactor

--- TheCanalaveLibrary.Core/ServiceInterfaces/IStoryOverviewService.cs
+++
@@ -1,9 +0,0 @@
-using TheCanalaveLibrary.Core.DTOs;
-
-namespace TheCanalaveLibrary.Core.ServiceInterfaces;
-
-public interface IStoryOverviewService
-{
-    Task<StoryDetailsDTO?> GetStoryByIdAsync(int storyId);
-    Task<int> GetRandomNumber();
-}
--- TheCanalaveLibrary.Core/ServiceInterfaces/IStoryService.cs
+++
@@ -1,8 +0,0 @@
-using TheCanalaveLibrary.Core.DTOs;
-
-namespace TheCanalaveLibrary.Core.ServiceInterfaces;
-
-public interface IStoryService
-{
-    Task<StoryDetailsDTO?> GetStoryByIdAsync(int storyId);
-}
